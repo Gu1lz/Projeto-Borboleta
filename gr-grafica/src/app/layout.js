18dbbb1fc3c0c5ecc9e7503c8b74cbf76a48a5fc
Gu1lz <162931725+Gu1lz@users.noreply.github.com>
2025-11-13 23:28:56 +0000

    denovo readmd

--- gr-grafica/src/app/layout.js
+++ gr-grafica/src/app/layout.js
@@ -1,7 +1,8 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "GR Soluções Gráficas - 25 Anos de Experiência"
+  title: "GR Soluções Gráficas - 25 Anos de Experiência",
+  description: "GR Soluções para todo público!",
 };
 
 export default function RootLayout({ children }) {
